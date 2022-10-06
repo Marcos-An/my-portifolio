a6274e61b7435c34682efe19b8781e303ea1fc5c
Marcos-An <loseonjr@gmail.com>
2022-10-06 09:25:06 -0400

    home redirect

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,8 +1,11 @@
 import styles from './logo.module.scss'
+import { useRouter } from 'next/router'
 
 export function MyLogo() {
+  const router = useRouter()
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={() => router.push('/')}>
       <div className={styles.circle} />
       <div className={styles.content}>
         <h4>MARCOS ANTONIO</h4>
